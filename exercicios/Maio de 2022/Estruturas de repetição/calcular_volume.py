08260b74ba96c7dfc144084c51460a03e11c1976
Cyberleitor <cyberleitor@gmail.com>
2022-05-10 17:42:15 -0300

    Mudança em calcula_volume

--- exercicios/Maio de 2022/Estruturas de repetição/calcular_volume.py
+++ exercicios/Maio de 2022/Estruturas de repetição/calcular_volume.py
@@ -1,20 +1,6 @@
 import time
 
-print('Informe o comprimento da caixa...')
-comprimento_caixa = float(input())
-print('Informe a largura da caixa...')
-largura_caixa = float(input())
-print('Informe a altura da caixa...')
-altura_caixa = float(input())
-
-volume_caixa = comprimento_caixa * largura_caixa * altura_caixa
-
-print('O volume da caixa é: ',volume_caixa,'.')
-
-time.sleep(1)
-
-print('Você deseja fazer um novo cálculo de volume (s/n)?')
-resposta = input()
+resposta = 's'
 
 while resposta == 's':
 
