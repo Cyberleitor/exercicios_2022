76851a5f80445ac53864ef5af181bf432ae72bc2
Cyberleitor <9pa1iokh@duck.com>
2022-10-17 09:05:26 -0300

    Python and POO — study

---
+++ exercicios/outubro_de_2022/python_and_poo/bank_account.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import datetime
+
+class Cliente:
+
+	def __init__(self, nome, sobrenome, cpf):
+		self.nome = nome
+		self.sobrenome = sobrenome
+		self.cpf = cpf
+
+class Conta:
+
+	def __init__(self, numero, cliente, saldo):
+		self.numero = numero
+		self.cliente = cliente
+		self.saldo = saldo
+		self.historico = Historico()
+
+	def depositar(self, valor):
+		self.saldo += valor
+		self.historico.transacoes.append("Depósito de {}".format(valor))
+
+	def sacar(self, valor):
+		if self.saldo < valor:
+			return False
+		else:
+			self.saldo -= valor
+			self.historico.transacoes.append("Saque de {}".format(valor))
+
+	def extrato(self):
+		print("Número: {} \nSaldo: {}".format(self.numero, self.saldo))
+		self.historico.transacoes.append("Tirou extrato - Saldo de {}".format(self.saldo))
+
+	def transferir(self, destino, valor):
+		retirar = self.sacar(valor)
+		if retirar == False:
+			return False
+		else:
+			destino.depositar(valor)
+			self.historico.transacoes.append("Transferência de {} para a conta {}".format(valor, destino.numero))
+			return True
+
+class Historico:
+
+	def __init__(self):
+		self.data_abertura = datetime.datetime.today()
+		self.transacoes = []
+
+	def imprimir(self):
+		print(f"Data de abertura: {self.data_abertura}")
+		print("Transações: ")
+		for transacao in self.transacoes:
+			print("-", transacao)
+
+cliente_um = Cliente('João', 'Oliveira', '11111111111-11')
+cliente_dois = Cliente('José', 'Azevedo', '222222222-22')
+conta_um = Conta('123-4', cliente_um, 1000.0)
+conta_dois = Conta('123-5', cliente_dois, 1000.0)
+conta_um.depositar(100.0)
+conta_um.sacar(50.0)
+conta_um.transferir(conta_dois, 200.0)
+conta_um.extrato()
+conta_um.historico.imprimir()
+conta_dois.historico.imprimir()
+
